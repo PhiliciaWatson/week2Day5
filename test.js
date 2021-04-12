3474f3b56e100ba473fd1d4aaf471951977eac1c
PhiliciaWatson <philiciawatson7@gmail.com>
2021-04-12 05:24:19 -0400

    first commit

---
+++ test.js
@@ -0,0 +1,27 @@
+alert('Hello World');
+// Javascript is case sensitive
+ myVar = 10;
+// Assign variable
+var myVar2 = 5;
+myVar += 5;
+// adds one
+myVar++
+
+// addition variable
+myVar = myVar + 10;
+myVar += 10;
+
+// substration
+myVar = myVar -5;
+myVar -=5;
+
+// multiply
+myVar = myVar *10;
+myVar *10;
+
+// divide 
+myVar = myVar / 2;
+myVar /= 2;
+
+// concatinate
+"Hello" + "Philicia"
